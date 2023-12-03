9b5de7938f372bd903a6c8f9657c7b7d0cad9b3f
Iryna Kavalenka <iryna_kavalenka@epam.com>
2023-12-03 16:01:48 +0100

    update csv file import component and api import path

--- front-end/src/constants/apiPaths.ts
+++ front-end/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://h2edkcvx01.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://dgoalnfcfd.execute-api.eu-west-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };

1e02a1ab1f1de5fc7bdba8fd199b617fe2ed5d38
Iryna Kavalenka <iryna_kavalenka@epam.com>
2024-01-04 19:27:44 +0100

    replace ebs with gateway api

--- front-end/src/constants/apiPaths.ts
+++ front-end/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "http://cart-api-dev.eu-west-1.elasticbeanstalk.com",
+  order: "https://nox0ewqjxk.execute-api.eu-west-1.amazonaws.com/",
   import: "https://h2edkcvx01.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://dgoalnfcfd.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "http://cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://nox0ewqjxk.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;

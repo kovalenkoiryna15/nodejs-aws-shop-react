a45acaa061f33bbc69975c80f05a68b408d5efe9
Iryna Kavalenka <iryna_kavalenka@epam.com>
2024-01-07 00:24:55 +0100

    refactor

--- front-end/src/models/Product.ts
+++ front-end/src/models/Product.ts
@@ -1,11 +1,11 @@
 import * as Yup from "yup";
 
 export const ProductSchema = Yup.object({
-  id: Yup.string().required(),
+  id: Yup.string().required().default(""),
   title: Yup.string().required().default(""),
   description: Yup.string().default(""),
   price: Yup.number().positive().required().defined().default(0),
-  image: Yup.string(),
+  image: Yup.string().default(""),
 });
 
 export const AvailableProductSchema = ProductSchema.shape({

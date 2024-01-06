9e411f2721aaef71aa725cb0a965d162df169330
Iryna Kavalenka <52598991+kovalenkoiryna15@users.noreply.github.com>
2024-01-07 00:13:08 +0100

    Task 9 (#6)

--- front-end/src/constants/apiPaths.ts
+++ front-end/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://3nc3bgubn8.execute-api.eu-west-1.amazonaws.com/prod",
+  order: "https://nox0ewqjxk.execute-api.eu-west-1.amazonaws.com/",
   import: "https://h2edkcvx01.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://dgoalnfcfd.execute-api.eu-west-1.amazonaws.com/prod",
-  cart: "https://3nc3bgubn8.execute-api.eu-west-1.amazonaws.com/prod/api",
+  cart: "https://nox0ewqjxk.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
